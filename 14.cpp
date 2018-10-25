ee88cf338422c53322da389af60fc45c20ec00c9
hieu <hieupm2k@gmail.com>
2018-10-25 20:45:35 +0700

    Update 14.cpp
    bai 14

---
+++ 14.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	bool a = true;
+	if(""){
+		printf("dung");
+	}
+	else{
+		printf("sai");
+	}
+	// neu ketquadieukien >=0 thi ra true con sai thi false
+	// 0 thi false khac 0 la true
+}
